cdd0d889dcbf4bab0f1b1243e8dd4f439bba1acb
hwangJi-dev <melon2228@swu.ac.kr>
2023-03-13 17:20:49 +0900

    feat: gRPC 통신 코드 및 UI 코드 작성

--- grpc-prac/grpc-prac/Screen/Main/Controller/ViewController.swift
+++ grpc-prac/grpc-prac/Screen/Main/Controller/ViewController.swift
@@ -8,45 +8,104 @@
 import UIKit
 import GRPC
 import NIO
-import NIOSSL
+import SnapKit
 
 final class ViewController: UIViewController {
-    let textField = UITextField()
+    private let textField = UITextField()
+    private let getHelloButton = UIButton()
+    private let resultLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        Task {
-            await getGRPC()
-        }
-    }
-    
-    func getGRPC() async {
-            let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-            let channel = try? GRPCChannelPool.with(
-                target: .host("localhost", port: 1234),
-                transportSecurity: .plaintext,
-                eventLoopGroup: group
-            )
-            
-            defer {
-                try! channel?.close().wait()
-            }
-            
-            // Provide the connection to the generated client.
-            let greeter = Helloworld_GreeterAsyncClient(channel: channel!)
-            
-            // Form the request with the name, if one was provided.
-            let request = Helloworld_HelloRequest.with {
-                $0.name = "jieun"
-            }
-            
-            do {
-                let greeting = try await greeter.sayHello(request)
-                print("Greeter received: \(greeting.message)")
-            } catch {
-                print("Greeter failed: \(error)")
-            }
+        setupUI()
+        configureUI()
     }
 }
 
+// MARK: - UI
+extension ViewController {
+    private func setupUI() {
+        // textField
+        textField.font = UIFont.systemFont(ofSize: 13.0, weight: .regular)
+        textField.placeholder = "인사하고 싶은 사람의 이름을 입력하세요."
+        textField.borderStyle = .roundedRect
+        
+        // getHelloButton
+        getHelloButton.setTitle("👆🏻인사하기", for: .normal)
+        getHelloButton.backgroundColor = .lightGray
+        getHelloButton.layer.cornerRadius = 5.0
+        getHelloButton.addTarget(self, action: #selector(getHelloButtonDidTap), for: .touchUpInside)
+        
+        // resultLabel
+        resultLabel.font = UIFont.systemFont(ofSize: 13.0, weight: .semibold)
+        resultLabel.textColor = .brown
+    }
+    
+    private func configureUI() {
+        self.view.addSubview(textField)
+        self.view.addSubview(getHelloButton)
+        self.view.addSubview(resultLabel)
+        
+        textField.snp.makeConstraints {
+            $0.topMargin.equalTo(50)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.centerX.equalToSuperview()
+        }
+        
+        getHelloButton.snp.makeConstraints {
+            $0.top.equalTo(textField.snp.bottom).offset(10)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(100)
+        }
+        
+        resultLabel.snp.makeConstraints {
+            $0.top.equalTo(getHelloButton.snp.bottom).offset(100)
+            $0.centerX.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Custom Methods
+extension ViewController {
+    @objc
+    private func getHelloButtonDidTap() {
+        Task {
+            let greetingMessage = await getGRPC(textField.text ?? "")
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.resultLabel.text = greetingMessage
+                self?.textField.text = ""
+            }
+        }
+    }
+}
+
+// MARK: - gRPC server connect
+extension ViewController {
+    private func getGRPC(_ name: String) async -> String {
+        let group = MultiThreadedEventLoopGroup(numberOfThreads: 3)
+        let channel = try? GRPCChannelPool.with(
+            target: .host("localhost", port: 1234),
+            transportSecurity: .plaintext,
+            eventLoopGroup: group
+        )
+        
+        defer {
+            try! channel?.close().wait()
+        }
+        
+        let greeter = Helloworld_GreeterAsyncClient(channel: channel!)
+        let request = Helloworld_HelloRequest.with {
+            $0.name = name
+        }
+        
+        do {
+            let greeting = try await greeter.sayHello(request)
+            print("Greeter received: \(greeting.message)")
+            return greeting.message
+        } catch {
+            print("Greeter failed: \(error)")
+            return ""
+        }
+    }
+}
